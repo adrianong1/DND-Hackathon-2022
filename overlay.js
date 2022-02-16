35bd96e523e18e5cf3f1c5929da5fb4665b8398a
Agustin <leeagustin@hotmail.com>
2022-02-16 14:55:26 -0800

    Overlay now displays acronym and its definitions

--- overlay.js
+++ overlay.js
@@ -1,6 +1,13 @@
-let overlay = document.createElement("div");
-overlay.setAttribute("id", "definition-overlay");
-document.body.appendChild(overlay);
+let overlayContainer = document.createElement("div");
+overlayContainer.setAttribute("id", "definition-overlay");
+let acronymContainer = document.createElement("div");
+acronymContainer.setAttribute("id", "acronym");
+let definitionListContainer = document.createElement("ol");
+definitionListContainer.setAttribute("id", "definitions-list");
+
+overlayContainer.appendChild(acronymContainer);
+overlayContainer.appendChild(definitionListContainer);
+document.body.appendChild(overlayContainer);
 
 window.onclick = (event) => {
     // hide overlay if user clicks outside of overlay
@@ -12,10 +19,21 @@ window.onclick = (event) => {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // move overlay to the bottom left corner of the selected text
     selectedTextRect = window.getSelection().getRangeAt(0).getBoundingClientRect();
-    overlay.style.top = selectedTextRect.bottom + "px";
-    overlay.style.left = selectedTextRect.right + "px";
+    overlayContainer.style.top = selectedTextRect.bottom + "px";
+    overlayContainer.style.left = selectedTextRect.right + "px";
 
-    // replace overlay text and display
-    overlay.innerHTML = request.text;
-    overlay.style.display = "block";
+    // setup overlay contents
+    acronymContainer.innerHTML = request.text;
+
+    definitionListContainer.innerHTML = ""; // clear list
+    definitions = [request.text, request.text, request.text];
+    for (const definition of definitions) {
+        let definitionContainer = document.createElement("li");
+        definitionContainer.classList.add("definition");
+        definitionContainer.innerHTML = definition + "'s definition";
+        definitionListContainer.append(definitionContainer);
+    }
+
+    // display overlay
+    overlayContainer.style.display = "block";
 })
