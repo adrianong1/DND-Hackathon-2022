e6fe87fc0abab5ed61e775d7f7df4dbc56566921
Agustin <leeagustin@hotmail.com>
2022-02-16 12:43:04 -0800

    Added extra option to get definition in context menu - Context menu appears when there is selected text and it's right-clicked

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.contextMenus.create({
+    id: "definition",
+    title: "Get Definition",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.onClicked.addListener((data, tab) => {
+    console.log(data)
+    console.log(tab)
+})
