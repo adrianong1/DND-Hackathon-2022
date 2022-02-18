a4b3a6123bf154e9f96603f4132bf67ef3170bf3
Agustin <leeagustin@hotmail.com>
2022-02-18 13:27:47 -0800

    Removed change color test button

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,3 @@
-let color = '#3aa757';
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
-
 // open help page on install
 chrome.runtime.onInstalled.addListener((reason) => {
     if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,6 +1,3 @@
-// When the button is clicked, inject setPageBackgroundColor into current page
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
 let englishCheckbox = document.getElementById("English");
 let frenchCheckbox = document.getElementById("French");
 
@@ -19,24 +16,6 @@ frenchCheckbox.addEventListener('change', e => {
         //search in french
     }
 });
-
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: setPageBackgroundColor,
-    });
-});
-
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
-}
 
 
 // toggle dark mode switch depending on dark mode setting
